5332007d8a8c2d28f9849081a0d1f2d4ff0777e3
Meinadia <meimeinadia764@gmail.com>
2025-03-21 05:30:54 +0700

    JS04_Praktikum 2.1_L6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::find(1);
+        $user = UserModel::firstWhere('level_id', 1);
         return view('user', ['data' => $user]);
+
     }
 }
