ccb08b611a090c47756e184ce2df3b9d3d3cba13
Meinadia <meimeinadia764@gmail.com>
2025-03-21 06:12:24 +0700

    JS04_Praktikum 2.6_Tambah user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
     }
 }
