82c1c40120206b82dc565d3d0cbb39c15454afba
Meinadia <meimeinadia764@gmail.com>
2025-03-21 04:16:27 +0700

    JS03_P4_L5

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
-use App\Http\Controllers\LevelController;
-use Illuminate\Support\Facades\Route;
+namespace App\Http\Controllers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+class LevelController extends Controller
+{
+    public function index()
+    {
+        $row = DB::insert('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
 
-Route::get('/level', [LevelController::class, 'index']);
+        return 'Update data berhasil. Jumlah data yang di update: ' . $row . ' baris';
+    }
+}
