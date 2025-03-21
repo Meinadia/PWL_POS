94070643ecbb90d6ada3b83c79d6fa36f941d7b4
Meinadia <meimeinadia764@gmail.com>
2025-03-21 23:35:36 +0700

    JS05_P1_L14

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -89,12 +89,10 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset(path: 'adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset(path: 'adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="{{ asset(path: 'adminlte/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
