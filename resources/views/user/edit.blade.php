e0a884aee2f5290621e0608a34e98402ea4bbd7f
Meinadia <meimeinadia764@gmail.com>
2025-04-26 19:05:46 +0700

    Implementasi Foto Profil

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -14,9 +14,9 @@
             </div>
             <a href="{{ url('user') }}" class="btn btn-sm btn-default mt-2">Kembali</a>
         @else
-            <form method="POST" action="{{ url('/user/'.$user->user_id) }}" class="form-horizontal">
+            <form method="POST" action="{{ url('/user/'.$user->user_id) }}" class="form-horizontal" enctype="multipart/form-data">
                 @csrf
-                {!! method_field('PUT') !!} <!-- Tambahkan baris ini untuk proses edit yang butuh method PUT -->
+                {!! method_field('PUT') !!}
 
                 <div class="form-group row">
                     <label class="col-1 control-label col-form-label">Level</label>
@@ -70,6 +70,37 @@
                 </div>
 
                 <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Foto Profil</label>
+                    <div class="col-11">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input @error('profile_picture') is-invalid @enderror" 
+                                   id="profile_picture" name="profile_picture" accept="image/jpeg,image/png,image/jpg,image/gif">
+                            <label class="custom-file-label" for="profile_picture">Pilih file...</label>
+                            @error('profile_picture')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <small class="form-text text-muted">Maksimal 2MB (JPEG, PNG, JPG, GIF)</small>
+                        
+                        <div class="mt-3 text-center">
+                            @if($user->profile_picture)
+                                <img src="{{ asset('storage/profile_pictures/'.$user->profile_picture) }}" 
+                                     class="img-thumbnail rounded-circle" 
+                                     width="150" 
+                                     height="150"
+                                     id="profile-picture-preview">
+                            @else
+                                <img src="{{ asset('images/default-profile.png') }}" 
+                                     class="img-thumbnail rounded-circle" 
+                                     width="150" 
+                                     height="150"
+                                     id="profile-picture-preview">
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group row">
                     <div class="col-11 offset-1">
                         <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
                         <a class="btn btn-sm btn-default ml-1" href="{{ url('user') }}">Kembali</a>
@@ -82,7 +113,33 @@
 @endsection
 
 @push('css')
+<style>
+    .custom-file-label::after {
+        content: "Browse";
+    }
+    #profile-picture-preview {
+        object-fit: cover;
+    }
+</style>
 @endpush
 
 @push('js')
+<script>
+    // Show file name when selected
+    $(document).ready(function() {
+        $('.custom-file-input').on('change', function() {
+            let fileName = $(this).val().split('\\').pop();
+            $(this).next('.custom-file-label').addClass("selected").html(fileName);
+            
+            // Preview image
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#profile-picture-preview').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+    });
+</script>
 @endpush
